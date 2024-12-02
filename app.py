ea1f830b5df8f735d8e3993f1d11fefd8c4ab0ab
Dan LaManna <dan.lamanna@kitware.com>
2024-12-02 17:07:00 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Header
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root(hello: str = Header(None)):
+    if hello and hello.strip().lower() == "kitware":
+        return 42
+    elif not hello:
+        return "hello header not set"
+    else:
+        return "hello header not set to kitware"
