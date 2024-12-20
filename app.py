005dda41f4722b8b980724349d5f9c17768a7656
Dan LaManna <dan.lamanna@kitware.com>
2024-12-20 09:49:19 -0500

    change code

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = FastAPI()
 @app.get("/", response_class=PlainTextResponse)
 async def read_root(hello: str = Header(None)) -> str:
     if hello and hello.strip().lower() == "kitware":
-        return "KTLbqCUse"
+        return "beep-boop-greetings-human"
     elif not hello:
         return "hello header not set"
     else:
-        return "hello header not set to kitware"
+        return "hello header not set to Kitware (case insensitive)"
